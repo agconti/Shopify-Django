8ce7b454c9c1fb019d0d5aeeda691e5c4481cb26
agconti <agc11d@gmail.com>
2013-09-26 11:24:50 -0400

    updated home and login example to django 1.5

--- shopify_app/urls.py
+++ shopify_app/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-        url(r'^$', 'shopify_app.views.login'),
-        url(r'^authenticate/$', 'shopify_app.views.authenticate'),
-        url(r'^finalize/$', 'shopify_app.views.finalize'),
-        url(r'^logout/$', 'shopify_app.views.logout'),
+        url(r'^$', 'shopify_app.views.login', name="login"),
+        url(r'^authenticate/$', 'shopify_app.views.authenticate', name='authenticate'),
+        url(r'^finalize/$', 'shopify_app.views.finalize', name='finalize'),
+        url(r'^logout/$', 'shopify_app.views.logout', name='logout'),
 )
